f7409fb100f20e3e2e32b76e7ef0a901d53ca720
hyechan90 <hyechan90@naver.com>
2020-04-12 22:38:10 +0900

    add new pics & css

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('main.ejs')
-})
+  res.render('main.ejs');
+});
+
+app.get('/imsorry', (req, res) => {
+  res.render('imsorry.ejs');
+});
 
 app.listen(3000, () => {
-    console.log('Server Running...')
-})
+  console.log('Server Running...');
+});
