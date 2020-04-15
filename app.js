6acc14c20d7e909b874dcbfa4fedc78b9e1e41f0
hyechan90 <hyechan90@naver.com>
2020-04-15 15:14:50 +0900

    commit

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.use(express.static('public'));
 app.set('views', __dirname + '/public/views');
 app.set('view engine', 'ejs');
+
+app.use((req, res, next) => {
+  const ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
+  console.log(ip);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('main.ejs');
